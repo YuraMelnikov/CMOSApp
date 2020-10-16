b28dc999266697003d48239dc59a1f79da3d9fdf
YuraMelnikov <yury.melnikau@gmail.com>
2020-10-16 19:03:43 +0300

    -

--- CMOS/MainActivity.cs
+++ CMOS/MainActivity.cs
@@ -13,11 +13,14 @@ using System.Linq;
 using Android.Widget;
 using System;
 using Android.Views;
+using Symbol.XamarinEMDK;
+using System.IO;
+using System.Xml;
 
 namespace CMOS
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, EMDKManager.IEMDKListener
     {
         private TextView numberTNForOrderlist;
         private RecyclerView ordersRecyclerView;
@@ -29,6 +32,13 @@ namespace CMOS
         private ImageButton buttonAplay;
         private Android.Support.V7.Widget.Toolbar toolbarInputData;
 
+        private EMDKManager emdkManager = null;
+        private ProfileManager profileManager = null;
+        private EditText codeInput = null;
+
+        private Button button1 = null;
+        private int count;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -39,6 +49,23 @@ namespace CMOS
             buttonRemove = (ImageButton)FindViewById(Resource.Id.buttonRemove);
             buttonAplay = (ImageButton)FindViewById(Resource.Id.buttonAplay);
             toolbarInputData = (Android.Support.V7.Widget.Toolbar)FindViewById(Resource.Id.toolbarInputData);
+            codeInput = (EditText)FindViewById(Resource.Id.codeInput);
+
+
+            EMDKResults results = EMDKManager.GetEMDKManager(ApplicationContext, this);
+            if (results.StatusCode != EMDKResults.STATUS_CODE.Success)
+            {
+                codeInput.SetText("Failed", TextView.BufferType.Normal);
+            }
+            else
+            {
+                codeInput.SetText("00000", TextView.BufferType.Normal);
+            }
+
+            count = 0;
+            button1 = (Button)FindViewById(Resource.Id.button1);
+            button1.Click += delegate { button1.Text = string.Format("{0} clicks!", count++); };
+            button1.Click += delegate { ApplyProfile(); };
 
             toolbarInputData.Visibility = ViewStates.Invisible;
             buttonRemove.Visibility = ViewStates.Invisible;
@@ -47,6 +74,64 @@ namespace CMOS
             buttonRemove.Click += Btn_Click;
 
             InitializingOrdersList();
+        }
+
+        private void ApplyProfile()
+        {
+            if (profileManager != null)
+            {
+                EMDKResults results = profileManager.ProcessProfile("ClockProfile", ProfileManager.PROFILE_FLAG.Set, new String[] { "" });
+                if (results.StatusCode == EMDKResults.STATUS_CODE.Success)
+                {
+                    codeInput.Text = "applied";
+                }
+                else if (results.StatusCode == EMDKResults.STATUS_CODE.CheckXml)
+                {
+                    using (XmlReader reader = XmlReader.Create(new StringReader(results.StatusString)))
+                    {
+                        String checkXmlStatus = "Status:\n\n";
+                        while (reader.Read())
+                        {
+                            switch (reader.NodeType)
+                            {
+                                case XmlNodeType.Element:
+                                    switch (reader.Name)
+                                    {
+                                        case "parm-error":
+                                            checkXmlStatus += "Parm Error:\n";
+                                            checkXmlStatus += reader.GetAttribute("name") + " - ";
+                                            checkXmlStatus += reader.GetAttribute("desc") + "\n\n";
+                                            break;
+                                        case "characteristic-error":
+                                            checkXmlStatus += "characteristic Error:\n";
+                                            checkXmlStatus += reader.GetAttribute("type") + " - ";
+                                            checkXmlStatus += reader.GetAttribute("desc") + "\n\n";
+                                            break;
+                                    }
+                                    break;
+                            }
+                        }
+
+                        if (checkXmlStatus == "Status:\n\n")
+                        {
+                            codeInput.Text = "St Ok.";
+                        }
+                        else
+                        {
+                            codeInput.Text = checkXmlStatus;
+                        }
+
+                    }
+                }
+                else
+                {
+                    codeInput.Text = "faild" + results.StatusCode;
+                }
+            }
+            else
+            {
+                codeInput.Text = "is null";
+            }
         }
 
         private void Btn_Click(object sender, EventArgs e)
@@ -134,6 +219,44 @@ namespace CMOS
         void OnPositionClick(object sender, int position)
         {
 
+        }
+
+        public void OnClosed()
+        {
+            codeInput.Text = "failed";
+            if (emdkManager != null)
+            {
+                emdkManager.Release();
+                emdkManager = null;
+            }
+        }
+
+        public void OnOpened(EMDKManager emdkManager)
+        {
+            codeInput.Text = "Opened";
+            this.emdkManager = emdkManager;
+            try
+            {
+                profileManager = (ProfileManager)emdkManager.GetInstance(EMDKManager.FEATURE_TYPE.Profile);
+            }
+            catch (Exception e)
+            {
+                codeInput.Text = e.Message;
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (profileManager != null)
+            {
+                profileManager = null;
+            }
+            if (emdkManager != null)
+            {
+                emdkManager.Release();
+                emdkManager = null;
+            }
         }
     }
 }
